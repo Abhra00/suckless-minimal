b96a35905714aabb4fe61fc5c31bff28e5bfb242
Abhra <abhramondal9674@gmail.com>
2024-08-03 17:37:02 +0530

    Update config.def.h

--- dwm-6.5/config.def.h
+++ dwm-6.5/config.def.h
@@ -20,7 +20,7 @@ static int showbar           		 = 1;        /* 0 means no bar */
 static int topbar            		 = 1;        /* 0 means bottom bar */
 static int user_bh                       = 6;        /* 2 is the default spacing around the bar's font */
 static char dmenufont[]      		 = "IosevkaMayukaiCodepro:size=10";
-static const char *fonts[]         	 = { "IosevkaMayukaiCodepro:size=10" ,"Symbols Nerd Font:size=14" };
+static const char *fonts[]         	 = { "IosevkaMayukaiCodepro:size=10" ,"Symbols Nerd Font:size=12" };
 static char normbgcolor[]          	 = "#222222";
 static char normbordercolor[]      	 = "#444444";
 static char normfgcolor[]          	 = "#bbbbbb";
